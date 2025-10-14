e4f67757f6b2850dd8eb0969bf5366e9f3a183a0
Henry Nwagwu <obijrnwagwu@gmail.com>
2025-10-14 00:07:32 -0500

    added custom cursors. also added a couple of sections that aren't filled out yet

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        // utility: `font-jetbrains`
-        jetbrains: ['"JetBrains Mono"', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'monospace'],
-      },
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+            colors: {
+                'light-background': 'var(--light-background)',
+            },
+            cursor: {
+                'black': 'url(/cursors/black-cursor.svg), auto',
+                'blue': 'url(/cursors/blue-cursor.svg), auto',
+                'white': 'url(/cursors/white-cursor.svg), auto',
+                'black-dot': 'url(/cursors/black-dot-cursor.svg), auto',
+            },
+            fontFamily: {
+                // utility: `font-jetbrains`
+                jetbrains: [
+                    '"JetBrains Mono"',
+                    'ui-monospace',
+                    'SFMono-Regular',
+                    'Menlo',
+                    'Monaco',
+                    'monospace',
+                ],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
