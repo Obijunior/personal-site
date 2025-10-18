02db2b0e6ca0b8b944864ed3912c251a940c5ff2
Henry Nwagwu <obijrnwagwu@gmail.com>
2025-10-18 00:09:27 -0500

    now it's actually working

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   integrations: [
     svelte(),
-    tailwind(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
